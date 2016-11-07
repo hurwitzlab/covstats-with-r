e4431aa024b24ad2b55b1205db3aa0326e6134b8
Scott Daniel <scottdaniel25@gmail.com>
2016-11-07 15:59:43 -0700

    split main into mouse and bacteria analysis

---
+++ bacterial-DNA-coverage.R
@@ -0,0 +1,5 @@
+#from the alignment using fastq-taxoner on PATRIC ~30K genomes
+
+#just trying with 0.fasta (so sort of a random collection of 4gb worth of bacterial genomes)
+
+#get all the *.sams from bacterial dir
