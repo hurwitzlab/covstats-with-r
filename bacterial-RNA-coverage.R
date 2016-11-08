47259b1f146b5aa13df60ce820d0f32b1a01640a
Scott Daniel <scottdaniel25@gmail.com>
2016-11-07 17:57:10 -0700

    and bacterial rna coverage too!

---
+++ bacterial-RNA-coverage.R
@@ -0,0 +1,20 @@
+#bacterial RNA coverage
+#to the "golden standard" 1,944 genomes
+#probably the closest comparison to mouse-RNA-coverage
+
+#commands I ran on the supercomputer
+
+#merge all the samples
+#samtools merge -@ 6 merged.bam -b allbams
+
+#remove PCR duplicates
+#samtools rmdup merged.bam dupsremoved-merged.bam
+
+#calculate the coverage
+#samtools depth dupsremoved-merged.bam > dupsremoved.coverage
+
+setwd("~/allbact-out-coverage/")
+system("cut -f3 dupsremoved.coverage > justcov")
+system("egrep -v 0 justcov > justcov_nozeroes")
+pw <- scan("justcov_nozeroes")
+summary(pw)
